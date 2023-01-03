ca33e81faa8af7d96fae373d8d53f650f2a25781
pedrowiskutzki <wiskutzki@gmail.com>
2023-01-03 13:42:30 -0300

    feat(commit):fitst commit

---
+++ src/main/java/com/neki/projeto/nekiproject/NekiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.neki.projeto.nekiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NekiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NekiprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neki/projeto/nekiproject/NekiprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neki.projeto.nekiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NekiprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
