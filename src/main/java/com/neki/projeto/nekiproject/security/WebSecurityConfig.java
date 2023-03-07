fa1b782236d46d6937a60096b024544dbba0614a
Pedro Wiskutzki <pedro.wiskutzki@neki-it.com.br>
2023-03-07 18:00:18 -0300

    fix(Swagger):Adjustment Swagger

--- src/main/java/com/neki/projeto/nekiproject/security/WebSecurityConfig.java
+++ src/main/java/com/neki/projeto/nekiproject/security/WebSecurityConfig.java
@@ -14,59 +14,51 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity 
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private CustomUserDetailsService customUserDetailsService;
+     @Autowired
+     private CustomUserDetailsService customUserDetailsService;
 
-    @Autowired
-    private JWTFilter jwtFilter;
+     @Autowired
+     private JWTFilter jwtFilter;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-   
+     @Autowired
+     private PasswordEncoder passwordEncoder;
 
-    @Override
-   public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
-        authenticationManagerBuilder
-            .userDetailsService(customUserDetailsService)
-            .passwordEncoder(passwordEncoder);
-   }  
-   
-   @Bean
-   @Override
-   public AuthenticationManager authenticationManagerBean() throws Exception{
+     @Override
+     public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+          authenticationManagerBuilder
+                    .userDetailsService(customUserDetailsService)
+                    .passwordEncoder(passwordEncoder);
+     }
 
-        return super.authenticationManagerBean();
+     @Bean
+     @Override
+     public AuthenticationManager authenticationManagerBean() throws Exception {
 
-   }
+          return super.authenticationManagerBean();
 
-   
-   @Override
-   protected void configure(HttpSecurity http) throws Exception {
+     }
 
-   
-        http    
-            .cors().and().csrf().disable()
-            .exceptionHandling()
-            .and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .authorizeRequests()
-        
-             .antMatchers(HttpMethod.POST,  "/pessoa/login", "/pessoa")
-            .permitAll()
-            
-            .anyRequest()
-            .authenticated();
-             
-             
+     @Override
+     protected void configure(HttpSecurity http) throws Exception {
 
+          http
+                    .cors().and().csrf().disable()
+                    .exceptionHandling()
+                    .and()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.POST, "/pessoa/login", "/pessoa")
+                    .permitAll()
+                    .antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated();
 
-            
-             http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-   }
+          http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+     }
 }
-
